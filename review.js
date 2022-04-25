fc37f04abbca5c03dd1bd6f2a2fae3e1243780ab
Sarinah <102683850+applebottomjeanss@users.noreply.github.com>
2022-04-25 09:46:17 -0500

    Add files via upload

---
+++ review.js
@@ -0,0 +1,12 @@
+// const para = document.getElementById("para");
+// para.innerHTML = "Lorem Ipsum";
+//dom is on the browser, under the elements tab, you change the dom through your linked javascript file, the dom is the document object model/replica of your html file.
+//how to create an element? document.createElement("arg");
+
+//how to set attributes? .setAtribute("arg1", "arg2");
+//how to get an element? document.getElemntsById("arg")
+//how to create text for that element? name.innerHTML = "Lorem Ipbsum";
+//when you do these four things, remeber to save them as variables.
+//just use the let variable. 
+//appendChild(htmlelement);
+
